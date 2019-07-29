1439aa7780e42f1a527f537fcc2e40b55f1b852d
Yasushi SHINOHARA <yasushi.shinohara@gmail.com>
2019-07-30 01:02:45 +0900

    Stupid wrong sentences in the script was fixed.

--- work/do.sh
+++ work/do.sh
@@ -2,6 +2,6 @@
 ulimit -s unlimited
 export OMP_STACKSIZE=256G
 export OMP_NUM_THREADS=1
-#mpirun -np 1 ../src/1D-TDHF <<<'"./input.dat"' >log.log
-#mpirun -np 16 ../src/1D-TDHF <<<'"./input.dat"' >log.log
-mpirun -np 2 ../src/1D-TDHF <<<'"./input.dat"' >log.log
+#mpirun -np 1 ../src/mATTOHF <<<'"./input.dat"' >log.log
+#mpirun -np 16 ../src/mATTOHF <<<'"./input.dat"' >log.log
+mpirun -np 2 ../src/mATTOHF <<< '"./input.dat"' > log.log
